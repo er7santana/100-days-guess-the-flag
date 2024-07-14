669138cad4f85be27bdc033aa65ff6925434d849
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-07-14 12:55:06 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 14/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
